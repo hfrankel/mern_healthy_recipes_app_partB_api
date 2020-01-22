91120448d65093b95c182b997d46f06f8c67218c
Harry Frankel <hfranks92@gmail.com>
2020-01-23 10:26:29 +1100

    Updated formatting

--- controllers/recipe_controller.js
+++ controllers/recipe_controller.js
@@ -29,35 +29,47 @@ async function create (req, res) {
     res.send("success")
 }
 
-    async function index (req, res) {
-        let recipes = await RecipeModel.find()
-        res.render("recipe/index", { recipes })
-    }
+async function index (req, res) {
+    let recipes = await RecipeModel.find()
+    res.render("recipe/index", { recipes })
+}
 
-    function make (req, res) {
-        res.render("recipe/new")
-    }
+function make (req, res) {
+    res.render("recipe/new")
+}
 
-    async function show (req, res) {
+async function show (req, res) {
+    let { id } = req.params
+    let recipe = await RecipeModel.findById(id)
+    res.render("recipe/show/", { recipe })
+}
+
+async function destroy (req, res) {
+    let { id } = req.params
+    await RecipeModel.findByIdAndRemove(id)
+    res.send("successfully deleted")
+}
+
+async function edit (req, res) {
+    let { id } = req.params
+    let recipe = await RecipeModel.findById(id)
+    res.render("recipe/edit", { recipe })
+}
+
+async function update (req, res) {
+    let { title,
+        description,
+        yield,
+        prepTime,
+        cookTime,
+        ingredients,
+        steps,
+        // ratings,
+        tags,
+        image } = req.body
+        console.log(req.params) // remove after checking
         let { id } = req.params
-        let recipe = await RecipeModel.findById(id)
-        res.render("recipe/show/", { recipe })
-    }
-
-    async function destroy (req, res) {
-        let { id } = req.params
-        await RecipeModel.findByIdAndRemove(id)
-        res.send("successfully deleted")
-    }
-
-    async function edit (req, res) {
-        let { id } = req.params
-        let recipe = await RecipeModel.findById(id)
-        res.render("recipe/edit", { recipe })
-    }
-
-    async function update (req, res) {
-        let { title,
+        await RecipeModel.findByIdAndUpdate(id, { title,
             description,
             yield,
             prepTime,
@@ -66,21 +78,9 @@ async function create (req, res) {
             steps,
             // ratings,
             tags,
-            image } = req.body
-            console.log(req.params) // remove after checking
-            let { id } = req.params
-            await RecipeModel.findByIdAndUpdate(id, { title,
-                description,
-                yield,
-                prepTime,
-                cookTime,
-                ingredients,
-                steps,
-                // ratings,
-                tags,
-                image })
-            res.redirect(`/recipes/${id}`)
-    }
+            image })
+        res.redirect(`/recipes/${id}`)
+}
 
 module.exports = {
     create,
