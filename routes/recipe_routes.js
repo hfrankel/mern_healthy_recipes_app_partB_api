a22ee18d9f7aa8aafa215a8498f432a4ebb1b47f
Harry Frankel <hfranks92@gmail.com>
2020-01-22 11:05:19 +1100

    Updated routes

---
+++ routes/recipe_routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const RecipeController = require('../controllers/recipe_controller');
+const CommentController = require('../controllers/comment_controller'); // check this
+
+router.get('/', RecipeController.index);
+
+router.post('/', RecipeController.create);
+
+router.get('/new', RecipeController.make);
+
+router.get('/:id', RecipeController.show);
+
+router.get('/:id/edit', RecipeController.edit);
+
+router.patch('/:id', RecipeController.update);
+
+router.put('/:id', RecipeController.update);
+
+router.delete('/:id', RecipeController.destroy);
+
+module.exports = router;
---
+++ routes/user_routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/user_controller');
+
+
+
+router.post('/', UserController.create);
+
+// router.get('/new', UserController.make);
+
+router.get('/:id', UserController.show);
+
+router.post('/:id', UserController.favourite);
+
+router.delete('/:id', UserController.destroy);
+
+module.exports = router;
