44dce9678bd705fe8f8bc55b051027d46d58f8f1
DB Thompson <dbthompson.au@gmail.com>
2020-02-05 09:53:52 +1100

    fixes for data

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,23 +1,23 @@
-const RecipeModel = require("./../database/models/recipe_model")
+const RecipeModel = require("./../database/models/recipe_model");
 
-async function create (req, res) {
-    // console.log("*******************");
-    // console.log(req.dbuser);
-    // console.log("*******************");
-    try {
-        let { id } = req.params
-        let { body } = req.body
-        let { nickname } = req.dbuser;
-        let recipe = await RecipeModel.findById(id)
-        console.log(recipe)
-        recipe.comments.push({body, nickname})
-        await recipe.save()
-        return res.json(recipe.comments[recipe.comments.length-1]); // send back the last comment in the comment array
-    } catch(err) {
-        return res.status(500).json(err);
-    }
+async function create(req, res) {
+  // console.log("*******************");
+  // console.log(req.dbuser);
+  // console.log("*******************");
+  try {
+    let { id } = req.params;
+    let { body } = req.body;
+    let { nickname } = req.dbuser;
+    let recipe = await RecipeModel.findById(id);
+    console.log(recipe);
+    recipe.comments.push({ body, nickname });
+    await recipe.save();
+    return res.json(recipe.comments[recipe.comments.length - 1]); // send back the last comment in the comment array
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 }
-
 module.exports = {
-    create
-}
+  create
+};
--- database/schemas/comment_schema.js
+++ database/schemas/comment_schema.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    body: {
-        type: String,
-        required: true
-    },
-    dateOfComment: {
-        type: Date,
-        default: Date.now()
-    },
-    nickname: {
-        type: String,
-        // required: true
-    }//,
-    // user_id: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: 'User',
-    //     required: true
-    // }
-})
+  body: {
+    type: String,
+    required: true
+  },
+  dateOfComment: {
+    type: Date,
+    default: Date.now()
+  },
+  nickname: {
+    type: String
+    // required: true
+  } //,
+  // user_id: {
+  //     type: Schema.Types.ObjectId,
+  //     ref: 'User',
+  //     required: true
+  // }
+});
 
 module.exports = CommentSchema;
