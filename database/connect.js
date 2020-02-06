65c131b93843d2bfa35bc06fb4754f45e44a3327
Harry Frankel <hfranks92@gmail.com>
2020-02-06 15:53:26 +1100

    Fixed connect db_host env variable

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/healthy_recipes_app", { useNewUrlParser: true });
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 mongoose.connection.on("error", console.log);
 
